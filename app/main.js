531fe6e445bcdd63a7133b57b620879ade16ac73
Liken Tan <likendev@gmail.com>
2024-04-14 02:00:17 +0800

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -5,19 +5,27 @@ console.log("Logs from your program will appear here!");
 
 const HTTP_STATUS = {
   200: "HTTP/1.1 200 OK\r\n\r\n",
-  404: "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-}
+  404: "HTTP/1.1 404 NOT FOUND\r\n\r\n",
+};
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const path = data.toString().split(" ")[1];
-    switch(path) {
-      case "/":
-        socket.write(HTTP_STATUS[200]);
-        break;
-      default:
-        socket.write(HTTP_STATUS[404]);
+    const [method, path] = data.toString().split(" ");
+
+    if (method === "GET" && path.startsWith("/echo")) {
+      const content = path.split("/echo/")[1];
+
+      socket.write("HTTP/1.1 200 OK\r\n");
+      socket.write("Content-Type: text/plain\r\n");
+      socket.write(`Content-Length: ${content.length}\r\n`);
+      socket.write(`\r\n${content}\r\n`);
+    }
+
+    if (path === "/") {
+      socket.write(HTTP_STATUS[200]);
+    } else {
+      socket.write(HTTP_STATUS[404]);
     }
   });
   socket.on("close", () => {
